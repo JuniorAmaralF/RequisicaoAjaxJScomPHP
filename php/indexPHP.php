f65f905fbbb3258e5d544e3408fb194a789ba3c1
Nazareno Ferreira Amaral Junior <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-07 15:20:56 -0300

    Delete indexPHP.php
    nao faz parte do relatorio

--- php/indexPHP.php
+++
@@ -1,8 +0,0 @@
-<?php
-$request_body = file_get_contents('php://input');
-
-$json_str = json_decode($request_body,false);
-var_dump($json_str);
-//$nome = $json_str['nome'];
-//$idade = $json_str['idade'];
-
